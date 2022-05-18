1da0e08fa597bb3b20b3465a2724142ab30d2cac
jose moncada <andresmonk2010@hotmail.com>
2022-05-18 08:53:25 -0500

    mi primer commit

---
+++ FEmaster/src/app/helpers/add-token.interceptor.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpErrorResponse
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { catchError} from 'rxjs/operators';
+import { error } from '@angular/compiler/src/util';
+
+@Injectable()
+export class AddTokenInterceptor implements HttpInterceptor {
+
+  constructor(private router:Router,private toastr:ToastrService) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      request = request.clone({ setHeaders: { Authorization: `Bearer ${token}`}});
+    }
+
+    
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse)=>{
+      if(error.status === 401){
+        this.toastr.error('Sesion expirada, por favor vuelva loguearse','Error!');
+        this.router.navigate(['/inicio/login'])
+      }
+      return throwError(error);
+    } ) );
+  }
+}
